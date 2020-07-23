162fbeb251cb6d70f44ced11d32bac3ff80e5af9
Jack Ross <43938405+JackRossProjects@users.noreply.github.com>
2020-07-23 11:49:46 -0600

    Update searching.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,21 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    # Base case check
+    # If last value >= first value, the array isnt sorted
+    if end >= start:
+        # Use first and last value to generate midpoint
+        mid = start + (end - start) // 2
+        # If midpoint is the target, success
+        if arr[mid] == target:
+            return mid
+        # if midpoint is greater than target, recurse through vaues on the right side
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, mid - start)
+        # If the midpoint is less than the target, recurse though the values on the left side
+        else:
+            return binary_search(arr, target, mid + start, end)
+      else:
+        return -1
 
 
 # STRETCH: implement an order-agnostic binary search
